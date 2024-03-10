5bcffe9379439a8d713da90d2c6882ce3244836d
samuel <kurdzsamuel@gmail.com>
2024-03-10 01:21:56 +0100

    added loader

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,18 @@ const pwaOptions: Partial<VitePWAOptions> = {
   includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
   manifest: {
     name: 'Endavu App',
+    id: 'Endavu',
     short_name: 'Endavu',
     lang: 'en-US',
     description: 'Financial assets viewer',
-    display: 'standalone',
+    display: 'fullscreen',
     display_override: ['window-controls-overlay'],
+    edge_side_panel: {
+      preferred_width: 496,
+    },
     theme_color: '#ffffff',
+    dir: 'ltr',
+    categories: ['finance', 'business'],
     icons: [
       {
         src: '/pwa-192x192.png',
